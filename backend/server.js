63f6b802b3098042dae0e61fa72ecca95d97aa8a
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-12-19 15:07:24 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL || process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
