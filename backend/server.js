6c2976fa9cfa56920eca4aff385c67519c8ce9f8
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-12-19 15:17:51 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-vercel-app.vercel.app'], // Add your Vercel URL
+  origin: ['http://localhost:5173', 'https://student-task-manager-wheat.vercel.app'], // Add your Vercel URL
   credentials: true
 }));
 app.use(express.json());
